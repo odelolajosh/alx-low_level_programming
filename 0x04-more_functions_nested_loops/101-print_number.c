19952c4a3e19727b7c924183c9e72281746d6df6
odelolajosh <oladapoodelola@gmail.com>
2022-03-17 19:05:06 +0000

    Add code

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+void print_number(int n)
+{
+	/* range start from 0 to the upper positive number limit */
+	unsigned int n1, hp;
+
+	/* Put '-' for negative values of n */
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	/* get the highest power for n */
+	n1 = n;
+	hp = 1;
+
+	while (n1 > 9)
+	{
+		n1 /= 10;
+		hp *= 10;
+	};
+
+	
+
+	for (; hp >= 1; hp /= 10)
+		_putchar(((n / hp) % 10) + '0');
+}
+
