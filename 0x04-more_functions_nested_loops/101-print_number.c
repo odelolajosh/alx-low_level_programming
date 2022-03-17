dc7bc2ce8083b3dc7927ccbe2211d8840abd90b0
odelolajosh <oladapoodelola@gmail.com>
2022-03-17 19:07:55 +0000

    Add docs

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * print_number - prints an integer
+ *
+ * @n: int to print
+ *
+ * Return: void
+ */
 void print_number(int n)
 {
 	/* range start from 0 to the upper positive number limit */
