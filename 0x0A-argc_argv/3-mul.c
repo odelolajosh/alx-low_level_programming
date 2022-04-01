46dbe209ff13c4268e894f3268f5b70bd4dfe1d7
odelolajosh <oladapoodelola@gmail.com>
2022-04-01 13:09:52 +0000

    Add Mandatory code

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers passed as arguments
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char* argv[])
+{
+	int a, b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (0);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	printf("%d\n", (a * b));
+	return (0);
+}
