66b2dd10398746cdd7d04e001bb91b0ff170eaf9
odelolajosh <oladapoodelola@gmail.com>
2022-04-05 09:42:55 +0000

    Add code for argstostr

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+char *argstostr(int ac, char **av)
+{
+	char *ptr;
+	int c, i, j, k;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (c = i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			c++;
+		c++;
+	}
+
+	ptr = malloc(sizeof(char) * (c + 1));
+
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	for (i = k = 0; (i < ac) && (k < c); i++, k++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, k++)
+		{
+			if (k >= c)
+				break;
+			ptr[k] = av[i][j];
+		}
+		if (k < c)
+			ptr[k] = '\n';
+	}
+	ptr[k] = '\0';
+
+	return ptr;
+}
+
