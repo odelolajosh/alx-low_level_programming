af29ab438f075771c2196628b6a8f2a642bc8527
odelolajosh <oladapoodelola@gmail.com>
2022-04-07 16:40:32 +0000

    Fix code

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,6 +18,8 @@ int *array_range(int min, int max)
 		return (NULL);
 
 	ptr = malloc(sizeof(*ptr) * (max - min + 1));
+	if (ptr == NULL)
+		return (NULL);
 
 	for (i = 0; min <= max; i++, min++)
 		ptr[i] = min;
