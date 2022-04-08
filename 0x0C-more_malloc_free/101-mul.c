eeb385de01019962b158bbdf6d4e8a686b7c6963
odelolajosh <oladapoodelola@gmail.com>
2022-04-08 00:27:25 +0000

    Add code

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * is_zero - exits if a number represented
+ * as string is zero
+ *
+ * @s: given string
+ */
+void is_zero(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i]; i++)
+		if (s[i] != '0')
+			return;
+	printf("0\n");
+	exit(0);
+}
+
+/**
+ * make_significant - remove zero from the start of a string
+ *
+ * @s: given string
+ * @len: len of string
+ *
+ * Return: a pointer to the significant version
+ */
+char *make_significant(char *s, int len)
+{
+	int i = 0, j = 0;
+	char *p;
+
+	while (s[i] == '0' && s[i] != '\0')
+		i++;
+
+	if (i == 0)
+		return (s);
+
+	p = malloc(sizeof(*s) * (len - i));
+	for (; s[i] != '\0'; i++, j++)
+		p[j] = s[i];
+	free(s);
+	return (p);
+}
+
+
+/**
+ * num_len - checks for the length of a number in base 10
+ * represented in a string
+ *
+ * @s: given string
+ *
+ * Return: the length of the number in string,
+ * exit program is string contains a non-digit
+ */
+int num_len(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+	return (i);
+}
+
+/**
+ * alloc_string - allocate and initialize
+ * string to a memory space
+ *
+ * @len: length of string
+ *
+ * Return: pointer to the string
+ */
+char *alloc_string(int len)
+{
+	char *ptr;
+	int i;
+
+	ptr = malloc(sizeof(*ptr) * len);
+	if (ptr == NULL)
+		printf("Error\n"), exit(98);
+
+	/* Initialize ptr */
+	for (i = 0; i < len; i++)
+		ptr[i] = '0';
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * passed as arguments
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int len1, len2, len, i, j, k, c = 0, ca = 0, pr, pa = 0, u = 0;
+	char *p;
+
+	if (argc != 3)
+		printf("Error\n"), exit(98);
+	len1 = num_len(argv[1]), len2 = num_len(argv[2]);
+	is_zero(argv[1]), is_zero(argv[2]), len = len1 + len2;
+	p = alloc_string(len);
+	for (i = len1 - 1, u = 0; i >= 0; i--, u++)
+	{
+		for (j = len2 - 1, k = len - 1 - u; j >= 0; j--, k--)
+		{
+			pr = ((argv[1][i] - '0') * (argv[2][j] - '0')) + c;
+			c = pr / 10;
+			pr %= 10;
+			pa = (p[k] - '0') + pr + ca;
+			ca = pa / 10;
+			pa %= 10;
+			p[k] = pa + '0';
+		}
+		p[k] = (c + ca + (p[k] - '0')) + '0';
+		c = 0, ca = 0;
+	}
+	p = make_significant(p, len);
+	printf("%s\n", p);
+	return (0);
+}
