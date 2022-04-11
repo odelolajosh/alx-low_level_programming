a95d02828097752e6ff1ad35584d66a96dfe4c3d
odelolajosh <oladapoodelola@gmail.com>
2022-04-11 23:50:57 +0000

    Add fixes

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,8 +10,8 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (!(d->name))
-		       printf("Name: (nil)\n");
+		if ((!d->name))
+			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
 
