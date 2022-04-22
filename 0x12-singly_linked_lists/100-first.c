fcfa3aadb2b73302ac810fcfc273fa39b910ca1c
odelolajosh <oladapoodelola@gmail.com>
2022-04-22 00:28:30 +0000

    [Fix] - betty style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * before_main - function executes before main
+ * b_main - function executes before main
  */
-void __attribute__((constructor)) before_main()
+void __attribute__((constructor)) b_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,15 @@
-section .data
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-section .text
+	SECTION .text
+	extern printf
 	global main
-
 main:
-	mov edx,len	;message length
-	mov ecx,msg	;message to write
-	mov ebx,1	;file descriptor (stdout)
-	mov eax,4	;system call number (sys_write)
-	int 0x80	;call kernel
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-	mov eax,1	;system call number (sys_exit)
-	int 0x80	;call kernel
-
-section .data
-msg db 'Hello, Holberton', 0xa	;string to be printed
-len equ $ - msg			;length of the string
+	mov eax, 0
+	ret
