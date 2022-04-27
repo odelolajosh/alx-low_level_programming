78e895507340a07aeb94ee8ecc0e471a88753139
odelolajosh <oladapoodelola@gmail.com>
2022-04-27 01:57:15 +0000

    Add code

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -67,3 +67,4 @@ size_t print_listint_safe(const listint_t *head)
 	free_listp(&hptr);
 	return (nnodes);
 }
+
--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -40,3 +40,4 @@ listint_t *find_listint_loop(listint_t *head)
 
 	return (NULL);
 }
+
