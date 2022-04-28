4de109eddf1805ff08483dc8a62493cdcfb59ebb
odelolajosh <oladapoodelola@gmail.com>
2022-04-28 21:16:42 +0000

    Add fixes

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,22 +1,27 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation
- * of a number.
- * @n: unsigned long int.
+ * get_bit - returns the value of a bit at a given
+ * index.
+ * @n: unsigned long int input.
+ * @index: index of the bit.
  *
- * Return: no return.
+ * Return: value of the bit.
  */
-void print_binary(unsigned long int n)
+int get_bit(unsigned long int n, unsigned int index)
 {
-	if (n >> 0)
+	unsigned int i;
+
+	if (n == 0 && index < 64)
+		return (0);
+
+	for (i = 0; i <= 63; n >>= 1, i++)
 	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
+		if (index == i)
+		{
+			return (n & 1);
+		}
 	}
-	else
-	{
-		_putchar('0');
-	}
+
+	return (-1);
 }
