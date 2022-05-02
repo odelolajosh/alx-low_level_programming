996d303a0b5957c994d73ff26893e46d21c3860b
odelolajosh <oladapoodelola@gmail.com>
2022-05-02 19:41:17 +0000

    Add code

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,88 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * exit_with_error - exits with error
+ *
+ * @message: error message
+ * @code: exit code
+ */
+void exit_with_error(char *message, int code)
+{
+	dprintf(STDERR_FILENO, "%s\n", message);
+	exit(code);
+}
+
+/**
+ * error_file_access - check if the files can be accessed
+ *
+ * @fd: file descriptor for file
+ * @filename: name of file source
+ * @action: action
+ * @code: exit code for error
+ */
+void error_file_access(int fd, char *filename, char *action, int code)
+{
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't %s file %s\n", action, filename);
+		exit(code);
+	}
+}
+
+/**
+ * error_file_close - checks if a file can be closed
+ *
+ * @fd: file descriptor for file
+ */
+void error_file_close(int fd)
+{
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - entry file
+ * copies content of a file to another file
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, rclose;
+	char buf[1024];
+	ssize_t bwrite, bread;
+
+	if (argc  != 3)
+		exit_with_error("Usage: cp file_from file_to", 97);
+
+	fd_from = open(argv[1], O_RDONLY);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file_access(fd_from, argv[1], "read from", 98);
+	error_file_access(fd_to, argv[2], "write to", 99);
+
+	bread = 1024;
+	while (bread == 1024)
+	{
+		bread = read(fd_from, buf, 1024);
+		if (bread == -1)
+			error_file_access(-1, argv[1], "read from", 98);
+		bwrite = write(fd_to, buf, bread);
+		if (bwrite == -1)
+			error_file_access(-1, argv[2], "write to", 99);
+	}
+
+	rclose = close(fd_from);
+	error_file_close(rclose);
+	rclose = close(fd_to);
+	error_file_close(rclose);
+
+	return (0);
+}
+
