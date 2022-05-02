4f1f8e5c6e53b27db542dfc0d022f728e189842e
odelolajosh <oladapoodelola@gmail.com>
2022-05-02 20:45:51 +0000

    Fix code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -63,23 +63,24 @@ int main(int argc, char *argv[])
 		exit_with_error("Usage: cp file_from file_to", 97);
 
 	fd_from = open(argv[1], O_RDONLY);
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file_access(fd_from, argv[1], "read from", 98);
+
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file_access(fd_to, argv[2], "write to", 99);
 
 	bread = 1024;
 	while (bread == 1024)
 	{
 		bread = read(fd_from, buf, 1024);
-		if (bread == -1)
-			error_file_access(-1, argv[1], "read from", 98);
+		error_file_access(bread, argv[1], "read from file", 98);
+
 		bwrite = write(fd_to, buf, bread);
-		if (bwrite == -1)
-			error_file_access(-1, argv[2], "write to", 99);
+		error_file_access(bwrite, argv[2], "write to", 99);
 	}
 
 	rclose = close(fd_from);
 	error_file_close(rclose);
+
 	rclose = close(fd_to);
 	error_file_close(rclose);
 
