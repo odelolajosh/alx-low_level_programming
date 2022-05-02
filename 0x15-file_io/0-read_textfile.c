eb39bc3a806bcaff721a7759c0fb6b58a1fc2c5d
odelolajosh <oladapoodelola@gmail.com>
2022-05-02 17:27:02 +0000

    Add function docs

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,12 +1,22 @@
 #include "main.h"
 
+/**
+ * read_textfile - reads a text file and prints it to the POSIX
+ * standard output
+ *
+ * @filename: file name
+ * @letters: numbers of letters to read and print
+ *
+ * Return: if successful return actual number of letters it could read
+ * and print otherwise return 0
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	char *buf;
 	ssize_t read_b, write_b;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
@@ -14,17 +24,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
 
 	read_b = read(fd, buf, letters);
-	if (read_b == -1)
-		return (0);
+	write_b = write(STDOUT_FILENO, buf, read_b);
 
 	close(fd);
-
-	write_b = write(STDOUT_FILENO, buf, read_b);
-	if (write_b == -1)
-		return (0);
-
 	free(buf);
 
 	return (write_b);
