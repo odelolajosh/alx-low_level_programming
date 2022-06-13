d100ef17ecf5529bf8d2ad4aba1f608b75e54a9c
odelolajosh <oladapoodelola@gmail.com>
2022-06-13 23:09:03 +0100

    [Update] - Concatenate LD_PRELOAD variable

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/odelolajosh/alx-low_level_programming/master/0x18-dynamic_libraries/not_random.so -o /tmp/not_random.so
-export LD_PRELOAD=/tmp/not_random.so
+export LD_PRELOAD=/tmp/not_random.so:$LD_PRELOAD
