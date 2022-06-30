c5738858a8d00ec6801cb7aa3cfdc07cf124ff5e
odelolajosh <oladapoodelola@gmail.com>
2022-07-01 00:07:47 +0100

    [Fix] - correct size for hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,6 +3,7 @@
 /**
  * hash_table_create - created a hash table with a definite size
  * @size: size of the hash table
+ *
  * Return: pointer to hash table if create otherwise NULL
  */
 hash_table_t *hash_table_create(unsigned long int size)
@@ -11,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t **arr;
 	unsigned long int i;
 
-	ht = malloc(sizeof(hash_node_t));
+	ht = malloc(sizeof(hash_table_t));
 	if (!ht)
 		return (NULL);
 
