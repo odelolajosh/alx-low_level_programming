f57cdbe585a1405205b72c8ccacc1d009ba0db6a
odelolajosh <oladapoodelola@gmail.com>
2022-06-30 22:39:54 +0100

    [Feature] - task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - created a hash table with a definite size
+ * @size: size of the hash table
+ * Return: pointer to hash table if create otherwise NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	hash_node_t **arr;
+	unsigned long int i;
+
+	ht = malloc(sizeof(hash_node_t));
+	if (!ht)
+		return (NULL);
+
+	arr = malloc(sizeof(hash_node_t *) * size);
+	if (!arr)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = NULL;
+
+	ht->size = size;
+	ht->array = arr;
+
+	return (ht);
+}
