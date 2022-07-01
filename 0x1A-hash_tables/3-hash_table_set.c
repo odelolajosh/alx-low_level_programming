a781ed0d8b7f39078632b43b851665c9d304aa61
odelolajosh <oladapoodelola@gmail.com>
2022-07-01 18:52:54 +0100

    [Fix] - free `value` to be replaced

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,6 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
+			free(current->value);
 			current->value = strdup(value);
 			return (1);
 		}
