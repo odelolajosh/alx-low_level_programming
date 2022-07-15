bae05cb01cc953af3de350494b3f4e5b3bc19b1c
odelolajosh <oladapoodelola@gmail.com>
2022-07-15 21:58:32 +0100

    [Fix] - skip sort for array < 2

--- sorting_algorithms/0-bubble_sort.c
+++ sorting_algorithms/0-bubble_sort.c
@@ -9,17 +9,21 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int temp, flag = 1;
 
-	for (i = 0; i < size; i++)
+	if (size <= 2)
+		return;
+
+	for (i = 0; i < size && flag; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (j = 0, flag = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				flag = 1;
 				print_array(array, size);
 			}
 		}
